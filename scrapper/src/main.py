faec76f150b23588a3ca8bbc27e685f2fed78283
Gian Luis Bolivar Diana <gianluisbolivar1@gmail.com>
2026-03-12 00:25:58 +0000

    feat: containerizing scrapper

--- scrapper/src/main.py
+++ scrapper/src/main.py
@@ -1,7 +1,6 @@
 from datetime import datetime
 
-from domain import Market
-from market_scrappers.scrapper import Scrapper
+from domain import Market, Product
 from market_scrappers.scrapper_factory import ScrapperFactory
 
 def main() -> None:
@@ -9,17 +8,15 @@ def main() -> None:
         Market(name='Mercadona', url='https://www.mercadona.es/'),
     ]
 
-    scrappers: list[Scrapper] = [
-        ScrapperFactory().create(market)
-        for market in markets
-    ]
-
     today = datetime.now().strftime('%Y-%m-%d')
     postal_code = '38320'
 
-    for scrapper in scrappers:
-        products = scrapper.scrape(postal_code)
-        with open(f'{scrapper.market.name.lower()}_{today}.csv', 'w', encoding='utf-8') as f:
+    for market in markets:
+        products: list[Product]
+        with ScrapperFactory().create(market) as scrapper:
+            products = scrapper.scrape(postal_code)
+
+        with open(f'data/{market.name.lower()}_{today}.csv', 'w', encoding='utf-8') as f:
             f.write('Name;Price;Quantity\n')
             for product in products:
                 f.write(f'{product.name};{product.price} €;{product.quantity}\n')
--- scrapper/src/market_scrappers/scrapper.py
+++ scrapper/src/market_scrappers/scrapper.py
@@ -1,14 +1,31 @@
+from __future__ import annotations
+
 from abc import abstractmethod
+from types import TracebackType
 
 from selenium.webdriver.firefox.webdriver import WebDriver
+from selenium.webdriver.firefox.options import Options
 
 from domain import Market, Product
 
 class Scrapper:
     def __init__(self, market: Market):
         self.market = market
-        self.driver = WebDriver()
+        options = Options()
+        options.add_argument("--headless")
+        self.driver = WebDriver(options=options)
 
     @abstractmethod
     def scrape(self, postal_code: str) -> list[Product]:
         pass
+
+    def __enter__(self) -> Scrapper:
+        return self
+
+    def __exit__(
+        self,
+        exc_type: type[BaseException] | None,
+        exc_val: BaseException | None,
+        exc_tb: TracebackType | None
+    ) -> None:
+        self.driver.quit()
